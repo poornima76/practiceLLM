aa3659903b4e238bc3c24469714fa99b1529413a
Poornima Marasini <poornima.mrsn76@gmail.com>
2023-07-03 14:58:23 +0545

    Working of streamlit coded

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import streamlit as st # for the easy showcase of the frontend
 # we create various functions that will allow the users to ask certain questions
 # for encryption and decryption of a word
 def ceaser_cypher(word):
-    word_input = ""
+    word_input = ''
     for letter in word:
         word_input += chr(ord(letter)+3)
     return word_input
 
 def decrypt(word):
-    decrypt_word = ""
+    decrypt_word = ''
     for letter in word:
         decrypt_word += chr(ord(letter)-3)
     return decrypt_word
@@ -47,3 +47,19 @@ tools = [
 
 memory = ConversationBufferMemory(memory_key = "chat_history")
 llm = OpenAI(temperature = 0, verbrose = True)
+agent_chain = initialize_agent(tools, llm, agent = "conversational-react-description", memory = memory, verbrose = True)
+
+st.header(":purple[Ask me and I'll respond :)]")
+user_input = st.text_input("You: ")
+#initialize the memory buffer
+if 'memory' not in st.session_state:
+    st.session_state['memory'] = ''
+# streamlit button
+if st.button('Sumbit'):
+    st.markdown(agent_chain.run(input = user_input))
+    #prints the memory buffer
+    # adds conversation history to the memory buffer
+    st.session_state['memory'] += memory.buffer
+    print(st.session_state['memory'])
+    
+
